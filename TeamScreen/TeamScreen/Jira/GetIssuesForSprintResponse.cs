201c8d06cd4148253b15500cc2d8c9a6c0310886
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-03-20 00:09:28 +0100

    Additional fields mapped

--- TeamScreen/TeamScreen/Jira/GetIssuesForSprintResponse.cs
+++ TeamScreen/TeamScreen/Jira/GetIssuesForSprintResponse.cs
@@ -7,11 +7,18 @@
 
     public class Issue
     {
+        public string Key { get; set; }
         public Fields Fields { get; set; }
     }
 
     public class Fields
     {
         public string Summary { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class Status
+    {
+        public string Name { get; set; }
     }
 }
