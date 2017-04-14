95bd6e15a2e6b83769f1675d2031a5d95dac24a4
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-04-14 20:56:39 +0200

    Initial atempt at loading settings

--- TeamScreen/TeamScreen/Controllers/SettingsController.cs
+++ TeamScreen/TeamScreen/Controllers/SettingsController.cs
@@ -1,12 +1,28 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TeamScreen.Models.Settings;
+using TeamScreen.Services.Settings;
 
 namespace TeamScreen.Controllers
 {
     public class SettingsController : Controller
     {
+        private readonly ISettingsService _settingsService;
+
+        public SettingsController(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<PartialViewResult> CoreSettings()
+        {
+            var coreSettings = await _settingsService.Get<CoreSettings>(Const.CorePluginName);
+            return PartialView(coreSettings);
         }
     }
 }
