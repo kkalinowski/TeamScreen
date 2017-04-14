ec2649de115ed23ae9653d81df11f4b1f9e819bc
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-04-14 20:48:16 +0200

    Added settings db logic

--- TeamScreen/TeamScreen/Data/Entities/Setting.cs
+++ TeamScreen/TeamScreen/Data/Entities/Setting.cs
@@ -1,12 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace TeamScreen.Data.Entities
+﻿namespace TeamScreen.Data.Entities
 {
     public class Setting
     {
         public int Id { get; set; }
         public string Plugin { get; set; }
-        public string Key { get; set; }
         public string Value { get; set; }
+
+        public static Setting Create(string plugin, string value)
+        {
+            return new Setting
+            {
+                Plugin = plugin,
+                Value = value
+            };
+        }
     }
 }
--- TeamScreen/TeamScreen/Services/Settings/SettingsService.cs
+++ TeamScreen/TeamScreen/Services/Settings/SettingsService.cs
@@ -1,23 +1,42 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using TeamScreen.Data;
+using TeamScreen.Data.Entities;
 
 namespace TeamScreen.Services.Settings
 {
     public interface ISettingsService
     {
-        T Get<T>(string plugin);
-        void Set<T>(string plugin, T value);
+        Task<T> Get<T>(string plugin) where T : class;
+        Task Set<T>(string plugin, T value) where T : class;
     }
 
     public class SettingsService : ISettingsService
     {
-        public T Get<T>(string plugin)
+        private readonly AppDbContext _db;
+
+        public SettingsService(AppDbContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
         }
 
-        public void Set<T>(string plugin, T value)
+        public async Task<T> Get<T>(string plugin) where T : class
         {
+            var setting = await _db.Settings.FirstOrDefaultAsync(x => x.Plugin == plugin);
+            var deserialized = JsonConvert.DeserializeObject(setting.Value, typeof(T));
+            return (T)deserialized;
+        }
 
+        public async Task Set<T>(string plugin, T value) where T : class
+        {
+            var jsonValue = JsonConvert.SerializeObject(value);
+            var existing = await _db.Settings.FirstOrDefaultAsync(x => x.Plugin == plugin);
+            if (existing != null)
+                existing.Value = jsonValue;
+
+            await _db.Settings.AddAsync(Setting.Create(plugin, jsonValue));
+            await _db.SaveChangesAsync();
         }
     }
 }
