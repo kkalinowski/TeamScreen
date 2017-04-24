ab243b505fb125e08f4b22af022f9a215e98a2c0
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-04-24 15:31:39 +0200

    Additional fields for Jira and TeamCity settings

--- TeamScreen/TeamScreen.Plugin.Jira/Models/JiraSettings.cs
+++ TeamScreen/TeamScreen.Plugin.Jira/Models/JiraSettings.cs
@@ -3,5 +3,8 @@
     public class JiraSettings
     {
         public string Url { get; set; }
+        public string Project { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
