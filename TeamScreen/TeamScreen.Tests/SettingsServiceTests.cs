e4e81da2c801b2a8b91e0448e16b77cc1d128131
kkalinowski@BASE <kkalinowski@outlook.com>
2017-04-26 23:13:48 +0200

    Added NSubstitute and started writing test for SettingsService

---
+++ TeamScreen/TeamScreen.Tests/SettingsServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using NSubstitute;
+
+namespace TeamScreen.Tests
+{
+    public class SettingsServiceTests
+    {
+        [Fact]
+        public void get_returns_default_settings_when_cant_find_them_in_db()
+        {
+
+        }
+    }
+}
