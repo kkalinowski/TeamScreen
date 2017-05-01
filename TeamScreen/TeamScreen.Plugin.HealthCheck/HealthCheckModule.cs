81b3ec4c51243e17d4ec9f5f0a63e54e5921e0e9
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-05-01 13:35:31 +0200

    Added WardenService with simplest check

--- TeamScreen/TeamScreen.Plugin.HealthCheck/HealthCheckModule.cs
+++ TeamScreen/TeamScreen.Plugin.HealthCheck/HealthCheckModule.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using TeamScreen.Plugin.Base;
+using TeamScreen.Plugin.HealthCheck.Integration;
 
 namespace TeamScreen.Plugin.HealthCheck
 {
@@ -8,6 +9,7 @@ namespace TeamScreen.Plugin.HealthCheck
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<HealthCheckPlugin>().As<IPlugin>().PreserveExistingDefaults();
+            builder.RegisterType<WardenService>().As<IWardenService>();
         }
     }
 }
---
+++ TeamScreen/TeamScreen.Plugin.HealthCheck/Integration/WardenService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Warden;
+using Warden.Core;
+using Warden.Watchers.Web;
+
+namespace TeamScreen.Plugin.HealthCheck.Integration
+{
+    public interface IWardenService
+    {
+        Task<List<IWardenCheckResult>> DoHealthChecks();
+    }
+
+    public class WardenService : IWardenService
+    {
+        public async Task<List<IWardenCheckResult>> DoHealthChecks()
+        {
+            var results = new List<IWardenCheckResult>();
+            var configuration = Configure(results);
+            var warden = WardenInstance.Create(configuration);
+
+            await warden.StartAsync();
+            return results;
+        }
+
+        private WardenConfiguration Configure(List<IWardenCheckResult> results)
+        {
+            return WardenConfiguration
+                .Create()
+                .AddWebWatcher("http://kkalinowski.net")
+                .SetHooks(x => x.OnIterationCompleted(iteration => results.AddRange(iteration.Results)))
+                .RunOnlyOnce()
+                .Build();
+        }
+    }
+}
