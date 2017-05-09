b634e64f4c5ce5937de1078dadfec436510b504a
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-05-09 12:48:47 +0200

    AppDbContext is now IdentityContext

--- TeamScreen/TeamScreen.Data/Context/AppDbContext.cs
+++ TeamScreen/TeamScreen.Data/Context/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TeamScreen.Data.Entities;
 
 namespace TeamScreen.Data.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<PluginSetting> PluginSettings { get; set; }
 
