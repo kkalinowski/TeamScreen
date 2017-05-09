38d15022e8348a77bf24b4f9df86213f4db9df89
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-05-09 16:48:35 +0200

    Settings for ProjectTeam

--- TeamScreen/TeamScreen.Plugin.ProjectTeam/Models/ProjectTeamSettings.cs
+++ TeamScreen/TeamScreen.Plugin.ProjectTeam/Models/ProjectTeamSettings.cs
@@ -4,6 +4,10 @@ namespace TeamScreen.Plugin.ProjectTeam.Models
 {
     public class ProjectTeamSettings : ISettings<ProjectTeamSettings>
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Techs { get; set; }
+
         public ProjectTeamSettings WithDefaultValues()
         {
             return this;
