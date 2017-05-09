1d1b14da54b41be0ae7ed469d63c376424892081
kkalinowski@BASE <kkalinowski@outlook.com>
2017-05-09 20:42:09 +0200

    Fixed migrations

---
+++ TeamScreen/TeamScreen.Data/Context/TemporaryDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace TeamScreen.Data.Context
+{
+    //use only for Migration creation - http://benjii.me/2016/05/dotnet-ef-migrations-for-asp-net-core/
+    //also that's why System.Collections.Immutable and System.Diagnostics.DiagnosticSource are referenced
+    public class TemporaryDbContextFactory : IDbContextFactory<AppDbContext>
+    {
+        public AppDbContext Create()
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseSqlite("Data Source=TeamScreen.sqlite");
+            return new AppDbContext(builder.Options);
+        }
+
+        public AppDbContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseSqlite("Data Source=TeamScreen.sqlite");
+            return new AppDbContext(builder.Options);
+        }
+    }
+}
