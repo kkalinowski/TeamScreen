a2604020eb36cde1b4969ba71217b73373bb49a0
Krzysztof Kalinowski <kkalinowski@outlook.com>
2017-05-13 19:31:06 +0200

    Trying to logout user

--- TeamScreen/TeamScreen/Controllers/ContainerController.cs
+++ TeamScreen/TeamScreen/Controllers/ContainerController.cs
@@ -2,22 +2,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TeamScreen.Data.Services;
 using TeamScreen.Models.Settings;
+using Microsoft.AspNetCore.Identity;
+using TeamScreen.Data.Entities;
+using TeamScreen.Models.Container;
 
 namespace TeamScreen.Controllers
 {
     public class ContainerController : Controller
     {
         private readonly ISettingsService _settingsService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ContainerController(ISettingsService settingsService)
+        public ContainerController(ISettingsService settingsService, UserManager<ApplicationUser> userManager)
         {
             _settingsService = settingsService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
         {
             var settings = await _settingsService.Get<CoreSettings>(Const.CorePluginName);
-            return View(settings);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return View(new ContainerModel { Settings = settings, User = user });
         }
     }
 }
---
+++ TeamScreen/TeamScreen/Models/Container/ContainerModel.cs
@@ -0,0 +1,11 @@
+﻿using TeamScreen.Data.Entities;
+using TeamScreen.Models.Settings;
+
+namespace TeamScreen.Models.Container
+{
+    public class ContainerModel
+    {
+        public ApplicationUser User { get; set; }
+        public CoreSettings Settings { get; set; }
+    }
+}
