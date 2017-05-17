64fa21ebca25699adb67a33cf5dabb587ccee4b9
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-05-17 17:35:36 +0200

    Created git models

--- TeamScreen/TeamScreen.Plugin.Git/Integration/GetCommitsResponse.cs
+++ TeamScreen/TeamScreen.Plugin.Git/Integration/GetCommitsResponse.cs
@@ -1,6 +1,28 @@
-﻿namespace TeamScreen.Plugin.Git.Integration
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TeamScreen.Plugin.Git.Integration
 {
     public class GetCommitsResponse
     {
+        public Commit[] Commits { get; set; }
+    }
+
+    public class Commit
+    {
+        [JsonProperty(PropertyName = "commit")]
+        public CommitInfo Info { get; set; }
+    }
+
+    public class CommitInfo
+    {
+        public string Message { get; set; }
+        public Committer Committer { get; set; }
+    }
+
+    public class Committer
+    {
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
     }
 }
