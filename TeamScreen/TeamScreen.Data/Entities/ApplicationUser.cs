bb3dee53808aca9c6358b3a959dbe2c1a9b93930
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-05-17 16:30:21 +0200

    Removed uneeded comment

--- TeamScreen/TeamScreen.Data/Entities/ApplicationUser.cs
+++ TeamScreen/TeamScreen.Data/Entities/ApplicationUser.cs
@@ -2,7 +2,6 @@
 
 namespace TeamScreen.Data.Entities
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
