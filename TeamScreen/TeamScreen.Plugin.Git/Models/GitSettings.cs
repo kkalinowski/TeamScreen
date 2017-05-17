c37819b15c600628349d09bcf376d73af09e2c7c
Krzysztof Kalinowski <krzysztof.kalinowski@makingwaves.com>
2017-05-17 17:24:17 +0200

    Git settings done

--- TeamScreen/TeamScreen.Plugin.Git/Models/GitSettings.cs
+++ TeamScreen/TeamScreen.Plugin.Git/Models/GitSettings.cs
@@ -4,6 +4,11 @@ namespace TeamScreen.Plugin.Git.Models
 {
     public class GitSettings : ISettings<GitSettings>
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Owner { get; set; }
+        public string Repository { get; set; }
+
         public GitSettings WithDefaultValues()
         {
             return this;
